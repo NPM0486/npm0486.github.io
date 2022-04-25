6ff39bccce42e7ec69a353ffd3899bba57121d2c
NPM0486 <nikolas.papenfus@wp.pl>
2022-04-25 20:35:43 +0200

    Logo and .php
    add

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset=utf-8"/>
+	<title>Nikolas Papenfus</title>
+	<meta name="description" content="This is my portfolio and what I did during school."/>
+	<meta name="keywords" content="nikolas,papenfus,school,szkola,zespol szkol, polkowice, informatyk"/>
+	<meta name="author" content="Nikolas Papenfus"/>
+</head>
+<body>
+<h1>Hello World!</h1>
+<h2>
+<a href="https://github.com/NPM0486/npm0486.github.io" target="_blank" title="source code">I'm hosted with GitHub Pages.
+</a>
+</h2>
+<img src="img/logo_prototyp.png"/>
+</body>
+</html>
