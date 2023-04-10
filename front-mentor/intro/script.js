ac3dc3345c13adf628817fefbdf9665ce6f7af5f
Nikolas Papenfus <91491717+NPM0486@users.noreply.github.com>
2023-04-10 16:15:19 +0200

    mentor intro valid fix

--- front-mentor/intro/script.js
+++ front-mentor/intro/script.js
@@ -1,4 +1,5 @@
 function validateForm() {
+    var check = 0;
     var x = document.forms["trial"]["first-name"].value;
         if (x == "") {
             document.getElementById("first-name").classList.add("error");
@@ -10,6 +11,7 @@ function validateForm() {
             document.getElementById("first-name").classList.remove("error");
             document.getElementById("fnameId").innerHTML = "";
             document.getElementById("fnameImg").style.display = "none";
+            check += 1;
         }
 
     var y = document.forms["trial"]["last-name"].value;
@@ -23,6 +25,7 @@ function validateForm() {
         document.getElementById("last-name").classList.remove("error");
         document.getElementById("lnameId").innerHTML = "";
         document.getElementById("lnameImg").style.display = "none";
+        check += 1;
         }
 
     var z = document.forms["trial"]["email"].value;
@@ -36,6 +39,14 @@ function validateForm() {
         document.getElementById("email").classList.remove("error");
         document.getElementById("emailId").innerHTML = "";
         document.getElementById("emailImg").style.display = "none";
+        check += 1;
+        }
+
+        if ( z.indexOf("@") == -1 || z.indexOf(".") == -1 ) {
+        document.getElementById("email").classList.add("error");
+        document.getElementById("emailId").innerHTML = "Looks like this is not an email";
+        document.getElementById("emailImg").style.display = "block";
+        check -= 1;
         }
 
     var a = document.forms["trial"]["password"].value;
@@ -49,9 +60,22 @@ function validateForm() {
         document.getElementById("password").classList.remove("error");
         document.getElementById("passwordId").innerHTML = "";
         document.getElementById("passwordImg").style.display = "none";
+        check += 1;
+        if (a.length < 8 || a.length > 32 || a.indexOf(" ") != -1) {
+            document.getElementById("password").classList.add("error");
+            document.getElementById("passwordId").innerHTML = "Password must be: min 8 characters, max 32 characters, no spaces";
+            document.getElementById("passwordImg").style.display = "block";
+            check -= 1;
+            }
         }
 
-    if (x == "" || y == "" || z == "" || a == "") {
-    return false;
+    if (check != 4) {
+        return false;
+    }
+
+    if (check == 4) {
+        alert("Thank you for submitting your form!");
+        window.location.reload();
+        return false;
     }
 }
